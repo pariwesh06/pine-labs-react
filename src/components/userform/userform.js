997736e4f094238f0e3fe3248201c8a3848ef399
unknown <pariwesh06>
2021-01-16 09:30:45 +0530

    handling checkboxes

--- src/components/userform/userform.js
+++ src/components/userform/userform.js
@@ -8,7 +8,8 @@ export class Userform extends React.Component {//MVC
             user: {
                 fname: 'Pariwesh1',
                 age: 30,
-                gender: 'Male'
+                gender: 'Male',
+                skills: []
             },
             roles: [],
             users: [],
@@ -36,73 +37,96 @@ export class Userform extends React.Component {//MVC
             });
     }
     handleEvent = (event) => {
-        this.setState({  //to rerender, call setState
-            user: Object.assign(this.state.user, { [event.target.name]: event.target.value })
-        });
-    }
-    deleteUser = function (index, userid) {
-        const decision = window.confirm('Are you sure??');
-        if (!decision) {
-            return;
-        }
-        const promise = BackendService.deleteUser(userid);
-        promise.done((response) => {
-            this.state.users.splice(index, 1);
-            this.setState({
-                users: this.state.users
-            })
-        });
-        promise.fail((error) => alert('Deletion failed'));
-        console.log(promise);
-    }
-    sortAge = (event) => {
-        console.log('sorted');
-        let order = this.state.sortOrder;
-        order = !order;
-        this.state.users.sort((user1, user2) => { //ascending
-            if (order) { 
-                return user1.age - user2.age; }
-            else {
-                return user2.age - user1.age;
+        if (event.target.type == 'checkbox') {
+            if (event.target.checked) {
+                //add values here
+                this.state.user[event.target.name].push(event.target.value);
+            } else {
+                //remove basis value       
+                let i = -1;
+                this.state.user[event.target.name].map((value, index) => {
+                    if (value == event.target.value) {
+                        i = index;
+                    }
+                });
+                if (i > -1) {
+                    this.state.user[event.target.name].splice(i, 1);
+                }
             }
-        });
-        this.setState({
-            users: this.state.users,
-            sortOrder: order
-        });
+            this.setState({
+                user: this.state.user
+            });
+        }
     }
-    render() {
-        const userModel = this.state.user;
-        return (
-            <div>
-                <input value={userModel.fname} name='fname' onChange={this.handleEvent} placeholder={this.props.label} style={{ background: this.props.color }} />
-                <input value={userModel.age} name='age' onChange={this.handleEvent}
-                    placeholder='first Name copy' style={{ background: this.props.color }} />
-                <input placeholder='salary' value={userModel.salary} onChange={this.handleEvent} name='salary'></input>
-                <input type='radio' checked='true' value='Male' onChange={this.handleEvent} name='gender' />Male
-                <input type='radio' value='Female' onChange={this.handleEvent} name='gender' />Female
-                {this.state.roles.map((role) => <div><input type='radio' value={role} onChange={this.handleEvent} name='role'></input>{role}</div>)}
-                <button onClick={this.save}>Save</button>
-                <table>
-                    <thead >
-                        <th>First Name</th>
-                        <th onClick={this.sortAge}> Age</th>
-                        <th> Salary</th>
-                        <th>Role</th>
-                    </thead>
-                    <tbody>
-                        {this.state.users.map((user, index) => {
-                            return <tr>
-                                <td>{user.fname}</td>
-                                <td>{user.age}</td>
-                                <td>{user.salary}</td>
-                                <td>{user.role}</td>
-                                <td><button onClick={this.deleteUser.bind(this, index, user.id)}>Delete</button></td>
-                            </tr>
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        )
+        deleteUser = function (index, userid) {
+            const decision = window.confirm('Are you sure??');
+            if (!decision) {
+                return;
+            }
+            const promise = BackendService.deleteUser(userid);
+            promise.done((response) => {
+                this.state.users.splice(index, 1);
+                this.setState({
+                    users: this.state.users
+                })
+            });
+            promise.fail((error) => alert('Deletion failed'));
+            console.log(promise);
+        }
+        sortAge = (event) => {
+            console.log('sorted');
+            let order = this.state.sortOrder;
+            order = !order;
+            this.state.users.sort((user1, user2) => { //ascending
+                if (order) {
+                    return user1.age - user2.age;
+                }
+                else {
+                    return user2.age - user1.age;
+                }
+            });
+            this.setState({
+                users: this.state.users,
+                sortOrder: order
+            });
+        }
+        render() {
+            const userModel = this.state.user;
+            return (
+                <div>
+                    <input value={userModel.fname} name='fname' onChange={this.handleEvent} placeholder={this.props.label} style={{ background: this.props.color }} />
+                    <input value={userModel.age} name='age' onChange={this.handleEvent}
+                        placeholder='first Name copy' style={{ background: this.props.color }} />
+                    <input placeholder='salary' value={userModel.salary} onChange={this.handleEvent} name='salary'></input>
+                    <input type='radio' checked='true' value='Male' onChange={this.handleEvent} name='gender' />Male
+                    <input type='radio' value='Female' onChange={this.handleEvent} name='gender' />Female
+                    {this.state.roles.map((role) => <div><input type='radio' value={role} onChange={this.handleEvent} name='role'></input>{role}</div>)}
+
+                    <input value='Javascript' name='skills' onChange={this.handleEvent} type="checkbox" />Javascript
+                    <input value='Java' name='skills' onChange={this.handleEvent} type="checkbox" />Java
+                    <input value='React' name='skills' onChange={this.handleEvent} type="checkbox" />React
+
+                    <button onClick={this.save}>Save</button>
+                    <table>
+                        <thead >
+                            <th>First Name</th>
+                            <th onClick={this.sortAge}> Age</th>
+                            <th> Salary</th>
+                            <th>Role</th>
+                        </thead>
+                        <tbody>
+                            {this.state.users.map((user, index) => {
+                                return <tr>
+                                    <td>{user.fname}</td>
+                                    <td>{user.age}</td>
+                                    <td>{user.salary}</td>
+                                    <td>{user.role}</td>
+                                    <td><button onClick={this.deleteUser.bind(this, index, user.id)}>Delete</button></td>
+                                </tr>
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            );
+        }
     }
-}
