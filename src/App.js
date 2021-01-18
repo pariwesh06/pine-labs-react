7343505184718e5b18adc1a431265e8def9c8bfb
unknown <pariwesh06>
2021-01-18 08:49:25 +0530

    routing-demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 import { Userform } from "./components/userform/userform";
 function App() { //component
   return (  //JSX
     <div className="App">
-      <Userform label='First Name' color='white'></Userform>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="home" >Home</Link>
+            </li>
+            <li>
+              <Link to="userform" >Userform</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/userform">
+            <Userform></Userform>
+          </Route>
+        </Switch>
+      </Router>
+
 
     </div>
   );
