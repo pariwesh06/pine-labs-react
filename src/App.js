78c7817df147bc1e757becc561815b4d1153caec
unknown <pariwesh06>
2021-01-18 09:52:51 +0530

    added page not found support

--- src/App.js
+++ src/App.js
@@ -1,110 +1,45 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams
-} from "react-router-dom";
-
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-
+import { Link, Route, BrowserRouter as Router, Switch, useLocation } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import { Userform } from "./components/userform/userform";
+function App() { //component
+  return (  //JSX
+    <div className="App">
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="home" >Home</Link>
+            </li>
+            <li>
+              <Link to="userform" >Userform</Link>
+            </li>
+          </ul>
+        </nav>
         <Switch>
-          <Route path="/about">
-            <About />
+          <Route exact path="/home">
+            <Home></Home>
           </Route>
-          <Route path="/topics">
-            <Topics />
+          <Route path="/userform">
+            <Userform></Userform>
           </Route>
-          <Route path="/">
-            <Home />
+          <Route path="*">
+            <NoMatch></NoMatch>
           </Route>
         </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  let match = useRouteMatch(); //hook
-  return <h2>About
-    <ul>
-      <li>
-        <Link to={`${match.url}/Founders/122`}>Founders</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/address`}>
-          Address
-          </Link>
-      </li>
-    </ul>
-    <Switch>
-        <Route path={`${match.path}/:topicId/:subid`}>
-          <Topic />
-        </Route>
-        <Route path={match.path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
-  </h2>;
-}
-
-function Topics() {
-  let match = useRouteMatch(); //hook
-  console.log(match);
-  return (
-    <div>
-      <h2>Topics</h2>
-
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>
-            Props v. State
-          </Link>
-        </li>
-      </ul>
-
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
-          the page that is shown when no topic is selected */}
-      <Switch>
-        <Route path={`${match.path}/:topicId`}>
-          <Topic />
-        </Route>
-        <Route path={match.path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
+      </Router>
     </div>
   );
 }
+function NoMatch() {
+  let location = useLocation();
 
-function Topic() {
-  // let { topicId, subid } = useParams();
-  let params= useParams();
-
-  return <h3>Requested topic ID: {params.topicId}, {params.subid}</h3>;
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
 }
+export default App;
